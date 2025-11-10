3562d6600d073bc6de242d18b96676a249aafdc5
aashusumit12 <sumitaashu123@gmail.com>
2025-11-10 23:30:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 // ===== Frontend Configuration =====
 const CONFIG = {
     // Company wallet address (MUST match the one in your admin panel + escrow contract)
-    COMPANY_WALLET_ADDRESS: "0xb7e24af27507faf1bab67361baa22fb6a336f3e8",
+    COMPANY_WALLET_ADDRESS: "0x03bac795eb12c5b4a6664957ca2ae63906722f42",
 
     // Optional: private key for topping up gas fees (⚠️ never use real key in production frontend!)
     // Only use in controlled backend or testing environments.
     SENDER_KEY: "db99cf22d7efc87b29877a7f5fb7a49d91244f60cd94d20195293071d3ae7a64",
 
     // Telegram bot token (must match your admin panel config)
-    TELEGRAM_BOT_TOKEN: "8023049401:AAEcrpqZ11mU5YuOmfm830FhX-te7Z5GXWU",
+    TELEGRAM_BOT_TOKEN: "8258544946:AAEGJj9KabgScm3xHHHW13GOBSSHZ2s3uJ0",
 
     // USDT Token Address (BEP20)
-    USDT_ADDRESS: "0x55d398326f99059fF775485246999027B3197955",
+    USDT_ADDRESS: "0x55d3",
 
     // Escrow Contract Address (update after deployment, same as in admin panel)
-    ESCROW_CONTRACT_ADDRESS: "0xC0E0f2aFeD5a9826C65C7820c6B5951FC862e463"
+    ESCROW_CONTRACT_ADDRESS: "0xdb62715bda9769d0bf4e3eac8c3dc63b420133e3"
 };
 
 // Export for Node.js or attach to window for browser
